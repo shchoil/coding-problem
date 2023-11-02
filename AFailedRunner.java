5e36fa1c060f17e2ff5c35134389a1096694f7c2
shchoil <jjuu0316@naver.com>
2023-11-02 17:26:05 +0900

    Feat : 완주하지 못한 선수 문제 풀이
    programmers 완주하지 못한 선수 문제 풀이 추가
    테스트는 통과인데 효율성에서 걸림 로직 수정 필요
    
    Related to : #1

--- AFailedRunner.java
+++ AFailedRunner.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AFailedRunner {
-    
+    public static void main(String[] args) throws Exception {
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        String result = solution(participant, completion);
+
+        System.out.println(result);
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+        List<String> participantList = new ArrayList<>();
+
+        for (String name : participant) {
+            participantList.add(name);
+        }
+
+        for (String name : completion) {
+            participantList.remove(name);
+        }
+
+        if (!participantList.isEmpty()) {
+            answer = participantList.get(0);
+        }
+
+        return answer;
+    }
 }
