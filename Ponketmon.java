7e24e5bf8dad788e76e016553c67b30d4a248704
sanghoon.lee2 <sanghoon.lee2@samyang.com>
2023-11-02 15:03:34 +0900

    Refactor : 문제 풀이 수정
    programmers Ponketmon문제 풀이 수정 마무리하였습니다.
    
    Resolves: #1

--- Ponketmon.java
+++ Ponketmon.java
@@ -4,30 +4,17 @@ public class Ponketmon {
     public static void main(String[] args) throws Exception {
         int[] numbers = {3, 3, 3, 2, 2, 4};
         int result = solution(numbers);
-        System.out.println(result);
+        // System.out.println(result);
     }
 
     public static int solution(int[] nums) {
-        int answer = 0;
-        int maxNumLength = nums.length / 2;
+        int maxNum = nums.length / 2;
         HashSet<Integer> set = new HashSet<>();
 
-        generateNumberComb(nums, 0, 0, set);
-
-        answer = Math.min(set.size(), maxNumLength);
-
-        return answer;
-    }
-
-    private static void generateNumberComb(int[] nums, int idx, int currentNum, HashSet<Integer> set) {
-
-        if (idx == nums.length) {
-            set.add(currentNum);
-            return;
+        for (int num : nums) {
+            set.add(num);
         }
 
-        generateNumberComb(nums, idx + 1, currentNum, set);
-
-        generateNumberComb(nums, idx + 1, currentNum + nums[idx], set);
+        return Math.min(maxNum, set.size());
     }
 }
