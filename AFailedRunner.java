8cb86a4aa25c78fc9dd2ac3a102e85b0da8c42e4
shchoil <jjuu0316@naver.com>
2023-11-03 13:23:50 +0900

    Refactor : 완주하지 못한 선수 문제 풀이 수정
    programmers 완주하지 못한 선수 문제 풀이 수정
    효율성 테스트 이슈 해결 -> Hash의 getOrDefault을 사용하여 풀이
    
    Resolves : #1

--- AFailedRunner.java
+++ AFailedRunner.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AFailedRunner {
     public static void main(String[] args) throws Exception {
@@ -12,20 +14,42 @@ public class AFailedRunner {
 
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        List<String> participantList = new ArrayList<>();
+
+        Map<String, Integer> participantMap = new HashMap<>();
 
         for (String name : participant) {
-            participantList.add(name);
+            participantMap.put(name, participantMap.getOrDefault(name, 0) + 1);
         }
 
         for (String name : completion) {
-            participantList.remove(name);
+            participantMap.put(name, participantMap.get(name) - 1);
         }
 
-        if (!participantList.isEmpty()) {
-            answer = participantList.get(0);
+        for (String name : participant) {
+            if (participantMap.get(name) != 0) {
+                answer = name;
+                break;
+            }
         }
 
         return answer;
+
+        // 처음 풀이 - 효율성 테스트에서 걸림
+        // String answer = "";
+        // List<String> participantList = new ArrayList<>();
+
+        // for (String name : participant) {
+        //     participantList.add(name);
+        // }
+ 
+        // for (String name : completion) {
+        //     participantList.remove(name);
+        // }
+
+        // if (!participantList.isEmpty()) {
+        //     answer = participantList.get(0);
+        // }
+
+        // return answer;
     }
 }
